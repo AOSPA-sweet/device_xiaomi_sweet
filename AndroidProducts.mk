62ae21ba515f615f5ea5f7e55a109f5ead26c0d2
anupam41 <anupamsamanta41@gmail.com>
2022-11-20 13:49:17 +0530

    sweet: Init Reloaded

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sweet.mk
+    $(LOCAL_DIR)/reloaded_sweet.mk
+    
+COMMON_LUNCH_CHOICES := \
+    reloaded_sweet-user \
+    reloaded_sweet-userdebug \
+    reloaded_sweet-eng    
